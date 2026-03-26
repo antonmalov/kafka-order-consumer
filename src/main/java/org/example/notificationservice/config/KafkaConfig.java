14a66d4a7dbb3eb2571413a18140f214f1c97aec
Anton <maloanto@yandex.ru>
2026-03-26 16:22:21 +0300

    fix config

--- src/main/java/org/example/notificationservice/config/KafkaConfig.java
+++ src/main/java/org/example/notificationservice/config/KafkaConfig.java
@@ -17,15 +17,12 @@ import java.util.Map;
 @Configuration
 public class KafkaConfig {
 
-
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-
         config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
-
         return new DefaultKafkaProducerFactory<>(config);
     }
 
